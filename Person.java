ce4ebf60036922c8dbbffc76f675a34d908b665d
Jeff <woolridgejeff@proton.me>
2025-10-16 14:22:34 -0230

    Getters and Setters for Person.java

--- Person.java
+++ Person.java
@@ -8,5 +8,15 @@ public class Person {
         this.id = id;
         this.name = name;
         this.age = age;
-        this.phoneNumber = phoneNumber;
-}};
+        this.phoneNumber = phoneNumber;}
+
+        // Getters and setters
+    public String getId() { return id; }
+    public String getName() { return name; }
+    public int getAge() { return age; }
+    public String getPhoneNumber() { return phoneNumber; }
+
+    public void setName(String name) { this.name = name; }
+    public void setAge(int age) { this.age = age; }
+    public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; }
+    }
