3b8687e9188c25ea32889be9e144cae96a62beb1
Jeff <woolridgejeff@proton.me>
2025-10-16 14:19:06 -0230

    Initial Commit with some of Person class

---
+++ Person.java
@@ -0,0 +1,12 @@
+public class Person {
+    protected String id;
+    protected String name;
+    protected int age;
+    protected String phoneNumber;
+
+    public Person(String id, String name, int age, String phoneNumber) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.phoneNumber = phoneNumber;
+}};
